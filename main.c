72aef0fd3baeff2c802f38c2c43c94d7c5d51ebe
Miguel Cardoso <miguelcardoso1996@gmail.com>
2024-10-14 19:55:24 +0100

    welcome message

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
 int main(){
+//    printf("************************************\n");
+    printf("*********                   ********\n");
+    printf("********                     *******\n");
+    printf("*******                       ******\n");
+    printf("*****                          *****\n");
+    printf("****                            ****\n");
+    printf("***    Welcome to Tic Tac Toe!   ***\n");
+    printf("****                            ****\n");
+    printf("*****                          *****\n");
+    printf("*******                       ******\n");
+    printf("********                     *******\n");
+    printf("*********                   ********\n");
+//    printf("************************************\n");
+
+    printf("\n\n\n\n\n\n\n\n");
 
     printf("      1     |     2     |     3     \n");
     printf("- - - - - - - - - - - - - - - - - - \n");
     printf("      4     |     5     |     6     \n");
     printf("- - - - - - - - - - - - - - - - - - \n");
     printf("      7     |     8     |     9     \n");
+
+    printf("\n\n\n\n\n\n\n\n");
 
     printf("            |           |           \n");
     printf("- - - - - - - - - - - - - - - - - - \n");
