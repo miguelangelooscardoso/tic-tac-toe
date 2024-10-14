627ac2e1b4487ccdbb7a8fa263c78d8b8f213853
Miguel Cardoso <miguelcardoso1996@gmail.com>
2024-10-14 19:46:48 +0100

    empty and filled grid

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main(){
 
+    printf("      1     |     2     |     3     \n");
+    printf("- - - - - - - - - - - - - - - - - - \n");
+    printf("      4     |     5     |     6     \n");
+    printf("- - - - - - - - - - - - - - - - - - \n");
+    printf("      7     |     8     |     9     \n");
+
     printf("            |           |           \n");
     printf("- - - - - - - - - - - - - - - - - - \n");
     printf("            |           |           \n");
